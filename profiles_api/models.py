0083f22690ed13322f5bddf88593163f9ac4def7
Mukund R S <sit19cs040@sairamtap.edu.in>
2022-12-23 19:35:05 +0530

    added permissions and authentications only commit

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
-
+from django.conf import settings
 
 # Create your models here.
 
@@ -49,3 +49,15 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     def __str__(self):
         '''Return string represetation'''
         return self.email 
+
+class ProfileFeedItem(models.Model):
+    """Stores the status of each update and profile"""
+    user_profile= models.ForeignKey(
+        settings.AUTH_USER_MODEL ,
+        on_delete=models.CASCADE ,
+    )   
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def _str__(self):
+        return self.status_text
